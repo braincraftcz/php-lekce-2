e4db813332528b2796fca270b39b695c61093cf5
Jakub Kovaľ <kuba.koval@gmail.com>
2019-02-27 16:18:09 +0100

    Homework: triangle

--- pythagoras.php
+++ pythagoras.php
@@ -3,4 +3,10 @@ $a = 5;
 $b = 4;
 $obdelnikobsah = $a * $b;
 
-echo "Obsah obdelníku o stranách {$a}cm, {$b}cm je {$obdelnikobsah}cm2";
+echo "<p>Obsah obdelníku o stranách {$a}cm, {$b}cm je {$obdelnikobsah}cm<sup>2</sup></p>";
+
+$strana = 6;
+$uhel = 60;
+$obsahTrojuhelnika = round($strana * $strana * sin(deg2rad($uhel)) / 2);
+
+echo "<p>Rovnostranný trojúhelník o straně délky {$strana}cm a úhlu {$uhel} stupňů je {$obsahTrojuhelnika}cm<sup>2</sup></p>";
